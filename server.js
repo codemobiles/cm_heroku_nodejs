98e954e7fadedbc747091ec4e8ead4ad572dc7f9
codemobiles <chaiyasit.t@gmail.com>
2020-07-02 13:10:14 +0700

    init

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require("express")
 const app = express()
+const PORT = process.env.PORT || 8080
+
 
 app.get("/", (req, res)=>{
     res.json({result: "ok", data:[1,2,3,4,5]})
 })
 
-app.listen(8080, ()=>{
-    console.log("Serer is running.")
+app.listen(PORT, ()=>{
+    console.log(`Serer is running. ${PORT}`)
 })
